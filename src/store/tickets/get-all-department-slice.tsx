91585b38eacc78db536a6f081b6d26f97f56b4b1
FrontendSrdt <milisrivastava26@gmail.com>
2025-12-04 11:28:04 +0000

    changed dept endpoint

--- src/store/tickets/get-all-department-slice.tsx
+++ src/store/tickets/get-all-department-slice.tsx
@@ -15,7 +15,7 @@ const initialState: DepartmentState = {
 
 export const getAllDepartments = createAsyncThunk<any>("tickets/getAllDepartments", async (_, { rejectWithValue }) => {
   try {
-    const response = await coreservicesApi.get(`api/crm/core/core-service-ticket/department/${false}`);
+    const response = await coreservicesApi.get(`api/crm/core/core-service-ticket/department/${true}`);
     return response.data;
   } catch (error: any) {
     return rejectWithValue(error.response?.data.message || "Failed to fetch departments.");
